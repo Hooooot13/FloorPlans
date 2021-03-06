bec2473066d6aab028693d489c16e4d2b4a15b21
Levi Carroll <lcdx5000@gmail.com>
2021-03-06 10:02:50 -0600

    commit

---
+++ test.py
@@ -0,0 +1,27 @@
+############################################################
+#
+#       Title: FloorplanReader
+#
+#       Description: This program is ment to read floor plans 
+#       and then also read the venting file
+#       This program should also be able to interpret the scale of the image
+#       
+#
+#
+#
+#       Haiku:      It's snowing on mount
+#                   But the season is ending
+#                   Soon the rain will come
+#
+#
+############################################################
+
+
+
+import cv2 
+from pdf2image import convert_from_path
+
+
+FloorPlanPDF = "FloorPlanPDFs/###"
+
+FloorPlanImage = convert_from_path(FloorPlanPDF)
